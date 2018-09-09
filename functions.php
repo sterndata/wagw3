cde7833961e550662ed0a898bbd6bff81e0ec403
Steven Stern <steve@sterndata.com>
2018-09-09 11:19:21 -0500

    add column shortcode and style

--- functions.php
+++ functions.php
@@ -78,3 +78,23 @@ function wagw_flexslider_gallery_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'wagw_flexslider_gallery_scripts' );
+
+function shortcode_wagw_cols( $atts, $content ) {
+/*
+ * left, right, and center are for divs on three column pages
+ */
+
+	//move wpautop filter to AFTER shortcode is processed
+	remove_filter( 'the_content', 'wpautop' );
+	add_filter( 'the_content', 'wpautop' , 99 );
+	add_filter( 'the_content', 'shortcode_unautop', 100 );
+	$a = shortcode_atts( array(
+		'type' => 'center',
+	), $atts );
+	return '<div class="wagw_col wagw_col_'.sanitize_text_field( $a['type'] ).'">'.$content.'</div>';
+}
+add_shortcode( 'wagw_col','shortcode_wagw_cols' );
+function wagw_clear () {
+  return '<div style="clear: both;"></div>';
+}
+add_shortcode( 'clear', 'wagw_clear');
