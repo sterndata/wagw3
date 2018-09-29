2d37abd57993dc915959312ed897b69dd668bdae
Steven Stern <steve@sterndata.com>
2018-09-29 08:11:59 -0500

    phpcs

--- functions.php
+++ functions.php
@@ -118,3 +118,25 @@ function wpsites_add_logo_nav_menu( $menu, stdClass $args ) {
 
 	return $menu;
 }
+
+// Move Yoast to bottom
+function yoasttobottom() {
+				return 'low';
+}
+add_filter( 'wpseo_metabox_prio', 'yoasttobottom' );
+
+/*
+ * disable automatic jetpack share placement and move it
+ * to just above the footer (see footer.php)
+ */
+
+function jptweak_remove_share() {
+		remove_filter( 'the_content', 'sharing_display', 19 );
+		remove_filter( 'the_excerpt', 'sharing_display', 19 );
+	if ( class_exists( 'Jetpack_Likes' ) ) {
+		remove_filter( 'the_content', array( Jetpack_Likes::init(), 'post_likes' ), 30, 1 );
+	}
+}
+
+add_action( 'loop_start', 'jptweak_remove_share' );
+
