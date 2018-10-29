78bc2e5d24eac3f6c1decc8202770acfcd656f01
Steven Stern <steve@sterndata.com>
2018-10-29 08:44:49 -0500

    update ACF options, add lightbox template

--- grid-page.php
+++ grid-page.php
@@ -12,7 +12,13 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'grid' ); ?>
+				<?php
+				if( get_field( 'display_options' ) == 'link' ) {
+					get_template_part( 'content', 'grid' ); 
+				} else {
+					get_template_part( 'content', 'lightbox' );
+				}	
+				?>
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || '0' != get_comments_number() ) :
