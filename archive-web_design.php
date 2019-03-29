668eb8c8120b540b413a3d9c0d805db5ccbd5ca9
Sterndata <subscribed-lists@sterndata.com>
2019-03-29 11:30:12 -0500

    update archive page

--- archive-web_design.php
+++ archive-web_design.php
@@ -30,7 +30,31 @@ get_header(); ?>
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
+				<div id="grid" class="grid">
 
+				<?php
+					$image = get_field( 'grid_image' );
+					$slide_caption = get_field( 'caption' );
+					$slide_tag = get_field( 'tag' );
+					$slide_target = get_field( 'target' );
+					if ( $slide_target ) {
+						$href = '<a href="' . $slide_target . '" >';
+					}
+				$src_set = ' srcset ="' . wp_get_attachment_image_srcset( $image['id'] ) . '" ';
+				?>
+
+				<div class="box"><?php if ( $slide_target ) { echo $href; } ?><img src="<?php echo $image['url']; ?>" alt="<?php echo
+			 $image['alt']; ?>" <?php echo $src_set; ?> />
+					<?php
+						echo '<span class="slide_caption_wrapper">';
+						echo '<span class="slide_caption">'. $slide_caption . '</span><span class="slide_tag">' . $slide_tag . '</span>';
+					echo '</span>';
+					?>
+				<?php if ( $slide_target ) { echo '</a>'; } ?>
+
+						</div><!-- box -->
+
+				</div><!-- grid -->
 
 			endwhile;
 
