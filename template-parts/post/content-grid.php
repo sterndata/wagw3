a02d4266d9cfc6ae19542b29f6569d18da21097c
Sterndata <subscribed-lists@sterndata.com>
2019-03-29 11:11:38 -0500

    initial commit

---
+++ template-parts/post/content-grid.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package What A Great Website
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php if ( ! is_front_page() ) { ?>
+
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+	<?php
+	the_content();
+
+<div id="grid" class="grid">
+
+<?php
+	$image = get_ield( 'grid_image' );
+	$slide_caption = get_field( 'caption' );
+	$slide_tag = get_ield( '_tag' );
+	$slide_target = get_sub_field( 'target' );
+	if ( $slide_target ) {
+		$href = '<a href="' . $slide_target . '"';
+		if ( get_sub_field( 'new_window' ) ) {
+			if ( ! wp_is_mobile() ) {
+				$href .= ' target=_blank ';
+			}
+		}
+		$href .= ' >';
+	}
+	if ( $use_srcset ) {
+		$src_set = ' srcset ="' . wp_get_attachment_image_srcset( $image['id'] ) . '" ';
+	} else {
+		$src_set = '';
+	}
+?>
+<div class="box"><?php if ( $slide_target ) { echo $href; } ?><img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" <?php echo $src_set; ?> />
+	<?php
+	if ( $slide_caption ) {
+			echo '<span class="slide_caption">'. $slide_caption . '</span><span class="slide_tag">' . $slide_tag . '</span>';
+		}
+	echo '</span>';
+ ?>
+<?php if ( $slide_target ) { echo '</a>'; } ?>
+
+		</div><!-- box -->
+	<?php } // while have rows ?>
+</div><!-- grid -->
+<?php
+	} // images
+
+?>
+<?php } ?>
+	<div class="entry-content">
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'wagw' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+	<footer class="entry-footer">
+		<?php edit_post_link( __( 'Edit', 'wagw' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
